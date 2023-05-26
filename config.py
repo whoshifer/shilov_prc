09795e0acbcdcdc8de963d51f91baca05e29d1b0
who <103600299+whoshifer@users.noreply.github.com>
2023-05-26 18:52:41 +0500

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 settings = {
-    'TGTOKEN':'5715227963:AAErkjbb2lax5vNhSfGoNsEhMk1L4BVJ3JI',
-    'VKTOKEN':'52daf5cc52daf5cc52daf5ccb251c963fd552da52daf5cc36e0b817bc5423eff350db7d',
-    'GROUP_ID':'1014995',
-    'TOPIC_ID':'49013289',
+    'TGTOKEN':'API-key TG',
+    'VKTOKEN':'API-key VK',
+    'GROUP_ID':'',
+    'TOPIC_ID':'',
 }
 
 
